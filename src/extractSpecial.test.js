f0c566c46c9bc40e633dac53b49d0df6afdb15d3
“TatianaLevoshko” <delovoiy86@gmail.com>
2024-07-12 04:24:41 +0300

    commit

---
+++ src/extractSpecial.test.js
@@ -0,0 +1,42 @@
+import extractSpecial from './extractSpecial';
+
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+    },
+  ],
+};
+
+test('extractSpecial should return special attacks with default description', () => {
+  const result = extractSpecial(character);
+
+  expect(result).toEqual([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ]);
+});
